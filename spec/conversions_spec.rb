a17cf8acd626f1145a99b1a5af9a4c38ca25e7b5
victoriaehrbar <victoriaehrbar@gmail.com>
2020-10-21 00:51:16 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -1,10 +1,10 @@
 require_relative '../conversions.rb'
 
 describe "conversions" do
-  describe '#ounces_to_grams' do
+  describe '#grams_to_ounces' do
     it 'given zero, returns 0.0' do
-      grams = ounces_to_grams(0)
-      expect(grams).to eq(0.0)
+      ounces = grams_to_ouncess(0)
+      expect(ounces).to eq(0.0)
     end
   end
 end
